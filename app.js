cbc89eaaa456e3e58b0f88678ea7cd422a7dc978
sunmenghan2017 <1079352098@qq.com>
2020-02-28 08:11:01 +0800

    entropy

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 #!/usr/bin/node
 
-const add = require('./add');
+const transform = require('./entropy.js').transform,
+      h = require('./entropy.js').h,
+      sigma = require('./entropy.js').sigma,
+      validate = require('./entropy.js').validate,
+      roundFractional = require('./entropy.js').roundFractional;
 
-console.log(`30 + 40 = ${add(30, 40)}`);
+var p = [], i = 0;
 
-if(process.argv.lenght != 4){
-    console.log('add x y');
-    process.exit(1);
-}
-var x=Number(process.argv[2]),
-    y=Number(process.argv[3]);
-console.log(`x + y = ${add(x, y)}`);
+
+// #!/usr/bin/node
+
+// const add = require('./add');
+
+// console.log(`30 + 40 = ${add(30, 40)}`);
+
+// if(process.argv.lenght != 4){
+//     console.log('add x y');
+//     process.exit(1);
+// }
+// var x=Number(process.argv[2]),
+//     y=Number(process.argv[3]);
+// console.log(`x + y = ${add(x, y)}`);
